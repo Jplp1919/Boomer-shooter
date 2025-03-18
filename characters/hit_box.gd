ab728c72123d6ca1be21124f200af6838a7608b1
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2025-03-17 22:59:56 -0300

    machinegun added

--- characters/hit_box.gd
+++ characters/hit_box.gd
@@ -7,16 +7,15 @@ extends Area3D
 
 @export var health : int = 50
 
-
 #new code for dismemberment
 @export var Parent : Node3D
 @export var mesh : MeshInstance3D
-@export var linked_mesh : MeshInstance3D
 @export var bone : BoneAttachment3D
-@export var linked_bone : BoneAttachment3D
 @export var kill_parent_on_death : bool = false
 #new code for dismemberment
 
+@export var meshes_to_destroy: Array[MeshInstance3D]
+@export var bones_to_destroy: Array[BoneAttachment3D]
 signal on_hurt(damage_data: DamageData)
 
 func hurt(damage_data: DamageData):
@@ -29,11 +28,20 @@ func hurt(damage_data: DamageData):
 	if dead:
 		if mesh:
 			mesh.queue_free()
-		if linked_mesh:
-			linked_mesh.queue_free()
+			queue_free()
+		if meshes_to_destroy:
+			destroy_linked_meshes_and_bones()
 		if bone:
 			bone.queue_free()
-		if linked_bone:
-			linked_bone.queue_free()
+			
 		if kill_parent_on_death:
 			Parent.kill()
+	
+
+func destroy_linked_meshes_and_bones():
+	for mesh in meshes_to_destroy:
+		if mesh:
+			mesh.queue_free()
+	for bone in bones_to_destroy:
+		if bone:
+			bone.queue_free()
---
+++ effects/casings/7.92x_57.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var eject_speed := 4.0
+@export var gravity := 9.8
+@export var destroy_delay := 1.2
+@export var spin_speed := 180.0
+
+@onready var player = get_tree().get_first_node_in_group("player")
+
+var velocity := Vector3.ZERO
+var elapsed_time := 0.0
+
+func _ready():
+	var player_right = player.global_transform.basis.x
+	
+	velocity = (Vector3.UP * 2 + player_right).normalized() * eject_speed
+	await get_tree().create_timer(destroy_delay).timeout
+	queue_free()
+
+func _process(delta):
+	velocity.y -= gravity * delta
+	global_translate(velocity * delta)
+	rotate_x(deg_to_rad(spin_speed * delta))
