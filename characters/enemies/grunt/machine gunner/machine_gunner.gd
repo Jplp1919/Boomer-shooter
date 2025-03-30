f55a74ebe9bd5940e16bcd2fbef821fefef77bd8
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2025-03-30 18:24:15 -0300

    machine gunner developed

---
+++ characters/enemies/grunt/machine gunner/machine_gunner.gd
@@ -0,0 +1,52 @@
+extends Grunt
+
+var pickup_scene: PackedScene = preload("res://pickups/792x_57_pickup.tscn")
+var weapon_pickup_scene: PackedScene = preload("res://pickups/machinegun_pickup.tscn")
+var delete_meshes = false
+@export var meshes_to_destroy: Array[MeshInstance3D]
+
+func set_state(state: STATES):
+	if cur_state == STATES.DEAD:
+		return
+	cur_state = state
+	match cur_state:
+		STATES.ATTACK:
+			print("attack state set")
+		STATES.IDLE:
+			animation_player.play("idle")
+		STATES.STAGGERED:
+			if disarmed_l:
+				animation_player.play("dismembered_left_arm")
+				disarmed_l = false
+			else:
+				animation_player.play("stagger")
+		STATES.DEAD:
+			drop_pickup()
+			if beheaded:
+				animation_player.play("dismembered_head", 0.2)
+			else:
+				animation_player.play("die", 0.2)
+			collision_layer = 0
+			collision_mask = 1
+			ai_character_mover.stop_moving()
+
+func drop_pickup():
+	var chance = randf()  # Random value between 0.0 and 1.0
+	if chance <= 0.7:  # 70% chance (0.0 - 0.7)
+		spawn_pickup(pickup_scene)
+	elif chance <= 0.9:  # 20% chance (0.7 - 0.9)
+		spawn_pickup(weapon_pickup_scene)
+		delete_meshes = true
+	# else (0.9 - 1.0): 10% chance, do nothing
+	if delete_meshes and meshes_to_destroy:
+		destroy_meshes()
+
+func spawn_pickup(scene: PackedScene):
+	var pickup = scene.instantiate()
+	get_parent().add_child(pickup)
+	pickup.global_position = global_position
+
+func destroy_meshes():
+	for mesh in meshes_to_destroy:
+		if mesh:
+			mesh.queue_free()
--- characters/enemies/grunt/rifleman/rifleman.gd
+++ characters/enemies/grunt/rifleman/rifleman.gd
@@ -1,1 +1,34 @@
 extends Grunt
+
+var pickup_scene: PackedScene = preload("res://pickups/792x_57_pickup.tscn")
+
+func set_state(state: STATES):
+	if cur_state == STATES.DEAD:
+		return
+	cur_state = state
+	match cur_state:
+		STATES.ATTACK:
+			print("attack state set")
+		STATES.IDLE:
+			animation_player.play("idle")
+		STATES.STAGGERED:
+			if disarmed_l:
+				animation_player.play("dismembered_left_arm")
+				disarmed_l = false
+			else:
+				animation_player.play("stagger")
+		STATES.DEAD:
+			drop_pickup()
+			if beheaded:
+				animation_player.play("dismembered_head", 0.2)
+			else:
+				animation_player.play("die", 0.2)
+			collision_layer = 0
+			collision_mask = 1
+			ai_character_mover.stop_moving()
+
+func drop_pickup():
+	if randf() <= 0.7:
+		var pickup = pickup_scene.instantiate()
+		get_parent().add_child(pickup)
+		pickup.global_position = global_position
