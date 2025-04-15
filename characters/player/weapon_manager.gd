3857418b7f57f82a4369431c3bf42e26e23fc127
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2025-04-15 11:44:07 -0300

    debugging

--- characters/player/weapon_manager.gd
+++ characters/player/weapon_manager.gd
@@ -20,9 +20,9 @@ func _ready():
 			weapon.set_bodies_to_exclude([get_parent().get_parent().get_parent()])
 	disable_all_weapons()
 	for _i in range(weapons.size()):
-		weapons_unlocked.append(true) #unlocks all weapons, for test purposes
-		#weapons_unlocked.append(false) #locks all weapons by default
-		#weapons_unlocked[0] = true #sword unlocks by default
+		#weapons_unlocked.append(true) #unlocks all weapons, for test purposes
+		weapons_unlocked.append(false) #locks all weapons by default
+		weapons_unlocked[0] = true #sword unlocks by default
 	switch_to_weapon_slot(0)
 
 
---
+++ weapons/shotgun.gd
@@ -0,0 +1,41 @@
+extends Weapon
+
+@export var alt_recoil : Vector3
+@export var alt_attack_rate = 0.8
+
+
+
+func alt_attack(input_just_pressed: bool, input_held: bool):
+
+	if !input_held:
+		return
+	
+	if AmmoManager.get_ammo(ammo_type) == 0:
+		if input_just_pressed:
+			out_of_ammo.emit()
+			if out_of_ammo_sound:
+				ouf_of_ammo_sound_player.stream = out_of_ammo_sound
+				ouf_of_ammo_sound_player.play()
+		return
+	
+	var cur_time = Time.get_ticks_msec() / 1000.0
+	if cur_time - last_attack_time < alt_attack_rate:
+		return
+	
+	if ammo_type != AmmoType.NONE:
+		AmmoManager.use_ammo(ammo_type, 1)
+	
+	if !animation_controlled_attack:
+		actually_attack()
+	camera_holder.apply_recoil(alt_recoil)
+	last_attack_time = cur_time
+	animation_player.stop()
+	animation_player.play("alt_attack")
+	fired.emit()
+	#$"AttackSounds".play()
+	if muzzle_flash != null:
+		muzzle_flash.emitting = true
+
+func actually_attack():
+	attack_emmiter.global_transform = fire_point.global_transform
+	attack_emmiter.fire()
