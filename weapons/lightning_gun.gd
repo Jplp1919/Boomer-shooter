7f55808ca214f3a2d82a23f66802a122ff44eb62
Jplp1919 <88355957+Jplp1919@users.noreply.github.com>
2025-04-18 18:33:56 -0300

    added lightning gun, fine tuned the plasma pistol

---
+++ weapons/lightning_gun.gd
@@ -0,0 +1,61 @@
+extends Weapon
+
+@onready var sparks: GPUParticles3D = $Graphics/Sparks
+@onready var lightning: Node3D = $Graphics/Lightning
+
+var is_attacking := false
+
+func _ready() -> void:
+	attack_emmiter.set_damage(damage)
+	sparks.emitting = false
+	sparks.local_coords = true
+
+func attack(input_just_pressed: bool, input_held: bool):
+	if !automatic and !input_just_pressed:
+		return
+	if automatic and !input_held:
+		lightning.hide()  
+		is_attacking = false
+		return
+
+	if is_attacking:
+		return
+	
+	is_attacking = true
+	lightning.show()
+	
+	if AmmoManager.get_ammo(ammo_type) == 0:
+		if input_just_pressed:
+			out_of_ammo.emit()
+			if out_of_ammo_sound:
+				ouf_of_ammo_sound_player.stream = out_of_ammo_sound
+				ouf_of_ammo_sound_player.play()
+		lightning.hide()
+		is_attacking = false
+		return
+	
+	var cur_time = Time.get_ticks_msec() / 1000.0
+	if cur_time - last_attack_time < attack_rate:
+		lightning.hide()
+		is_attacking = false
+		return
+	
+	while input_held and is_attacking:
+		if ammo_type != AmmoType.NONE:
+			AmmoManager.use_ammo(ammo_type, 1)
+		
+		if !animation_controlled_attack:
+			actually_attack()
+		
+		camera_holder.apply_recoil(recoil)
+		last_attack_time = Time.get_ticks_msec() / 1000.0
+		animation_player.play("attack")
+		fired.emit()
+		sparks.emitting = true
+		
+		await get_tree().create_timer(attack_rate).timeout
+		if AmmoManager.get_ammo(ammo_type) == 0:
+			break
+	
+	lightning.hide()
+	is_attacking = false
---
+++ weapons/plasma_pistol.gd
@@ -0,0 +1,41 @@
+extends Weapon
+
+@onready var sparks: GPUParticles3D = $Graphics/Sparks
+@onready var trails: Node3D = $Graphics/Trails
+
+
+
+func attack(input_just_pressed: bool, input_held: bool):
+	if !automatic and !input_just_pressed:
+		return
+	if automatic and !input_held:
+		return
+	
+	if AmmoManager.get_ammo(ammo_type) == 0:
+		if input_just_pressed:
+			out_of_ammo.emit()
+			if out_of_ammo_sound:
+				ouf_of_ammo_sound_player.stream = out_of_ammo_sound
+				ouf_of_ammo_sound_player.play()
+		return
+	
+	var cur_time = Time.get_ticks_msec() / 1000.0
+	if cur_time - last_attack_time < attack_rate:
+		return
+	
+	if ammo_type != AmmoType.NONE:
+		AmmoManager.use_ammo(ammo_type, 1)
+	
+	if !animation_controlled_attack:
+		actually_attack()
+	camera_holder.apply_recoil(recoil)
+	last_attack_time = cur_time
+	animation_player.stop()
+	
+	animation_player.play("attack")
+	fired.emit()
+	#$"AttackSounds".play()
+	sparks.emitting = true
+	trails.show()
+	await get_tree().create_timer(0.1).timeout
+	trails.hide()
